79c5769300ba95551e21232c07e3794eea706a3b
lbonn041 <31964797+lbonn041@users.noreply.github.com>
2020-04-01 14:21:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 30;
+camera.position.z = 100;
 scene.add(camera)
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -32,11 +32,20 @@ var controls = new THREE.OrbitControls(camera, renderer.domElement);
 var focalPointGroup = new THREE.Group()
 focalPointGroup.add(camera)
 scene.add(focalPointGroup)
-focalPointGroup.position.y = -100
-focalPointGroup.rotation.x = Math.PI/3;
+//focalPointGroup.position.y = -100
+//focalPointGroup.rotation.x = Math.PI/3;
+
+var ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(ambientLight);
+
+var pointLight = new THREE.PointLight(0xff0000, 1, 0);
+pointLight.position.set(50, 200, 50);
+scene.add(pointLight);
+
 
 
 var geometry = new THREE.PlaneGeometry(cols, rows, cols, rows);
+
 var plane = new THREE.Group();
 var wireFrameMaterial = new THREE.MeshBasicMaterial({ color: 'black', wireframe: true });
 var faceMaterial = new THREE.MeshBasicMaterial({ color: 0x444444, vertexColors: THREE.VertexColors, wireframe: true, linewidth: 4});
@@ -75,9 +84,9 @@ function animate() {
 var update = function () {
 
     //update height of verticies
-    for (var i = 0; i < cols; i++) {
-        for (var j = 0; j < rows; j++) {
-            obj = geometry.vertices[j % Math.floor(rows) + i * Math.floor(rows)]
+    for (var i = 0; i < cols+1; i++) {
+        for (var j = 0; j < rows+1; j++) {
+            obj = geometry.vertices[j % Math.floor(rows+1) + i * Math.floor(rows+1)]
             obj.z = Math.abs(noise.perlin2((obj.x / 10) * step.turbulence, (obj.y / 10) * step.turbulence)) * step.height;
         }
     }
@@ -111,12 +120,12 @@ var update = function () {
 
 function addDatGui() {
     var gui = new dat.GUI();
-    gui.add(focalPointGroup.position, 'x', -50, 50).step(1).name("Inclinaison");
-    gui.add(focalPointGroup.position, 'y', -200, 10).step(1).name("Zoom");
-    gui.add(focalPointGroup.rotation, 'x', 0, 2*Math.PI/3).step(0.1).name("Vertical Rotation");
-    gui.add(plane.rotation, 'z', -1 * Math.PI, Math.PI).step(0.1).name("Horizontal Rotation");
-    gui.add(step, 'height', 0, 20);
-    gui.add(step, 'turbulence', 0, 3).step(0.0001);
+    //gui.add(focalPointGroup.position, 'x', -50, 50).step(1).name("Inclinaison");
+    //gui.add(focalPointGroup.position, 'y', -200, 10).step(1).name("Zoom");
+    //gui.add(focalPointGroup.rotation, 'x', 0, 2*Math.PI/3).step(0.1).name("Vertical Rotation");
+    gui.add(plane.rotation, 'z', -1 * Math.PI, Math.PI).step(0.1).name("h_rotation");
+    gui.add(step, 'height', 0, 50).name("Peak Height");
+    gui.add(step, 'turbulence', 0, 3).step(0.0001).name("Noise Turbulence");
     gui.add(faceMaterial, 'wireframe');
     gui.addColor(step, 'top_color').onChange(function (colorValue) {
         top_color = colorValue
@@ -131,8 +140,8 @@ function addDatGui() {
         renderer.setClearColor(colorValue)
     }).name("Background Colour");
 
-    gui.add(step, 'mid_height', step.low_height, 25.0).step(0.01).name("Top Height");
-    gui.add(step, 'low_height', 0.0, 15.0).step(0.01).name("Mid Height");
+    gui.add(step, 'mid_height', step.low_height, 50).step(0.01).name("Mid Max Value");
+    gui.add(step, 'low_height', 0.0, 50).step(0.01).name("Mid Lower Value");
 
 
     
