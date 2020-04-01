cc98d833b7c0c551ef81557838d1955566d23b95
Luc Bonnet <lbonn041@uottawa.ca>
2020-04-01 16:23:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // ==========================================================================
 // $Id: script.js,v 1 2020/04/01 20:04:25 lbonn041 
 // Assignment 4
@@ -10,18 +9,15 @@
 // ==========================================================================
 
 //Variables to set up plane Geometry
-var scale = 5;
-=======
 var scale = 5 ;
->>>>>>> 5319e71ad4b419896e0e032d6e1199f4dab8c6a8
 var cols = window.innerWidth / scale;
 var rows = window.innerHeight / scale;
 
 //Noise object from Noise.js
 var noise = new Noise(Math.random());
-
+ 
 //object with dat.gui propereties to be modified
-var GUI = function () {
+var GUI = function(){
     this.height = 5;
     this.turbulence = 0.0;
     this.low_height = 0.5;
@@ -60,40 +56,23 @@ scene.add(pointLight);
 var focalPointGroup = new THREE.Group()
 focalPointGroup.add(camera)
 scene.add(focalPointGroup)
-<<<<<<< HEAD
 focalPointGroup.position.y = -100
-focalPointGroup.rotation.x = Math.PI / 3;
+focalPointGroup.rotation.x = Math.PI/3;
 
 //plane group created to incluse PlaneGeometry and mesh
 //PlaneGeometry object created using the preset variables
-=======
-//focalPointGroup.position.y = -100
-//focalPointGroup.rotation.x = Math.PI/3;
-
-var ambientLight = new THREE.AmbientLight(0x404040); // soft white light
-scene.add(ambientLight);
-
-var pointLight = new THREE.PointLight(0xff0000, 1, 0);
-pointLight.position.set(50, 200, 50);
-scene.add(pointLight);
-
-
-
-var geometry = new THREE.PlaneGeometry(cols, rows, cols, rows);
-
->>>>>>> 5319e71ad4b419896e0e032d6e1199f4dab8c6a8
 var plane = new THREE.Group();
 var geometry = new THREE.PlaneGeometry(cols, rows, cols, rows);
 
 // MeshBasicMaterial to give object surface
-var faceMaterial = new THREE.MeshBasicMaterial({ color: 0x444444, vertexColors: THREE.VertexColors, wireframe: true });
-var wireFrameMaterial = new THREE.MeshBasicMaterial({ color: 0x141414, wireframe: true, visible: false });
+var faceMaterial = new THREE.MeshBasicMaterial({ color: 0x444444, vertexColors: THREE.VertexColors, wireframe: true});
+var wireFrameMaterial = new THREE.MeshBasicMaterial({ color: 0x141414, wireframe: true, visible:false});
 plane.add(new THREE.Mesh(geometry, faceMaterial));
 plane.add(new THREE.Mesh(geometry, wireFrameMaterial));
 scene.add(plane);
 
 
-wireMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, vertexColors: THREE.VertexColors, side: THREE.DoubleSide });
+wireMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 , vertexColors: THREE.VertexColors, side: THREE.DoubleSide });
 
 for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
@@ -121,18 +100,14 @@ var update = function () {
 
     //update height of verticies
     geometry.vertices.forEach(obj => {
-<<<<<<< HEAD
-        obj.z = Math.abs(noise.perlin2((obj.x / 10) * prop.turbulence, (obj.y / 10) * prop.turbulence)) * prop.height;
-=======
-        obj.z = Math.abs(noise.perlin2((obj.x / 10) * step.turbulence, (obj.y / 10) * step.turbulence)) * step.height; 
->>>>>>> 5319e71ad4b419896e0e032d6e1199f4dab8c6a8
+        obj.z = Math.abs(noise.perlin2((obj.x / 10) * prop.turbulence, (obj.y / 10) * prop.turbulence)) * prop.height; 
     });
     geometry.verticesNeedUpdate = true;
 
     //update color
-    for (let i = 0; i < geometry.faces.length; i = i + 2) {
+    for (let i = 0; i < geometry.faces.length; i=i+2) {
         var face = geometry.faces[i];
-        var face2 = geometry.faces[i + 1];
+        var face2 = geometry.faces[i+1];
         var z = geometry.vertices[face.a].z
         //face.color.setRGB(r/255 + z, g/255 + z, b/255 + z );
 
@@ -161,19 +136,12 @@ function addDatGui() {
     //gui.add(focalPointGroup.position, 'y', -200, 10).step(1).name("Zoom");
     //gui.add(focalPointGroup.rotation, 'x', 0, 2*Math.PI/3).step(0.1).name("Vertical Rotation");
     gui.add(plane.rotation, 'z', -1 * Math.PI, Math.PI).step(0.1).name("h_rotation");
-<<<<<<< HEAD
     gui.add(prop, 'height', 0, 50).name("Peak Height");
     gui.add(prop, 'turbulence', 0, 1).step(0.0001).name("Noise Turbulence");
-    gui.add(faceMaterial, 'wireframe').onChange(function () {
+    gui.add(faceMaterial, 'wireframe').onChange(function(){
         wireFrameMaterial.visible = !wireFrameMaterial.visible
     });
     gui.addColor(prop, 'top_color').onChange(function (colorValue) {
-=======
-    gui.add(step, 'height', 0, 50).name("Peak Height");
-    gui.add(step, 'turbulence', 0, 3).step(0.0001).name("Noise Turbulence");
-    gui.add(faceMaterial, 'wireframe');
-    gui.addColor(step, 'top_color').onChange(function (colorValue) {
->>>>>>> 5319e71ad4b419896e0e032d6e1199f4dab8c6a8
         top_color = colorValue
     }).name("Top Colour");
     gui.addColor(prop, 'mid_color').onChange(function (colorValue) {
@@ -186,16 +154,11 @@ function addDatGui() {
         renderer.setClearColor(colorValue)
     }).name("Background Colour");
 
-<<<<<<< HEAD
     gui.add(prop, 'mid_height', prop.low_height, 50).step(0.01).name("Mid Max Value");
     gui.add(prop, 'low_height', 0.0, 50).step(0.01).name("Mid Lower Value");
 
-=======
-    gui.add(step, 'mid_height', step.low_height, 50).step(0.01).name("Mid Max Value");
-    gui.add(step, 'low_height', 0.0, 50).step(0.01).name("Mid Lower Value");
->>>>>>> 5319e71ad4b419896e0e032d6e1199f4dab8c6a8
 
-
+    
     renderer.render(scene, camera);
 
 }
